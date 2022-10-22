93f47c3438651f1421116109e90cf8671fb52549
Nobuo1106 <gonnbe1986@gmail.com>
2022-10-22 20:29:37 +0900

    存在しないまちが入力された場合を考慮

--- Swift/6kyuu/Rainfawll.swift
+++ Swift/6kyuu/Rainfawll.swift
@@ -1,27 +1,33 @@
 func mean(_ d : String, _ town: String) -> Double {
-  var array:[String] = d.components(separatedBy: "\n")
+  if !machi.towns.contains(town) {
+    return -1.000000000000
+  }
+  let array:[String] = d.components(separatedBy: "\n")
   if let firstIndex = array.firstIndex(where: {$0.contains(town)}) {
-      var index = array[firstIndex].firstIndex(of: ":")
-      var ind = index?.utf16Offset(in: array[firstIndex])
+      let index = array[firstIndex].firstIndex(of: ":")
+      let ind = index?.utf16Offset(in: array[firstIndex])
       
       let d_str =  String(array[firstIndex].dropFirst(ind! + 1))
       let d_list = d_str.components(separatedBy: ",").map {Double($0.dropFirst(4))!} 
       return average(d_list)
   }
-    return 0.0
+    return -1.000000000000
 }
 
 func variance(_ d : String, _ town: String) -> Double {
-  var array:[String] = d.components(separatedBy: "\n")
+  if !machi.towns.contains(town) {
+    return -1.000000000000
+  }
+  let array:[String] = d.components(separatedBy: "\n")
   if let firstIndex = array.firstIndex(where: {$0.contains(town)}) {
-      var index = array[firstIndex].firstIndex(of: ":")
-      var ind = index?.utf16Offset(in: array[firstIndex])
+      let index = array[firstIndex].firstIndex(of: ":")
+      let ind = index?.utf16Offset(in: array[firstIndex])
       
       let d_str =  String(array[firstIndex].dropFirst(ind! + 1))
       let d_list = d_str.components(separatedBy: ",").map {Double($0.dropFirst(4))!} 
       return bunsan(d_list)
   }
-    return 0.0
+    return -1.000000000000
 }
 
 func sum(_ array:[Double])->Double{
@@ -37,4 +43,8 @@ func bunsan(_ array:[Double])->Double{
     let right=pow(average(array), 2.0)
     let count=array.count
     return (left-right)*Double(count/(count-1))
+}
+struct machi{
+  static let towns = ["Rome", "London", "Paris", "NY", "Vancouver", "Sydney", "Bangkok", "Tokyo",
+           "Beijing", "Lima", "Montevideo", "Caracas", "Madrid", "Berlin"]
 }
